97be05ca589a6b152e845b2b445075c2fb3437dd
Nicole Keeney <nicolejkeeney@gmail.com>
2025-11-24 16:16:40 -0800

    updates2

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,10 @@ function Footer() {
         <a href="https://github.com/nicolejkeeney" target="_blank" rel="noreferrer"><GitHubIcon/></a>
         <a href="https://www.linkedin.com/in/nicole-keeney/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
       </div>
-      <p>Created by Nicole Keeney • Original template by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Yuji Sato</a></p>
+      <p>Created by Nicole Keeney
+        <br/>Original website template by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer" style={{color: '#2E7DB8'}}>Yuji Sato</a>
+        <br/>Background artwork by <a href="https://chriskeeney.com" target="_blank" rel="noreferrer" style={{color: '#2E7DB8'}}>Chris Keeney</a>
+      </p>    
     </footer>
   );
 }
